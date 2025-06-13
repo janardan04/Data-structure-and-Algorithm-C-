fd99bdf599c4978f3e8c124fe545f79fe148db80
janardan04 <janardhanborse2003@gmail.com>
2025-06-13 09:24:22 +0530

    1306

--- Polynomial.c
+++ Polynomial.c
@@ -1,35 +1,97 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct {
-int coeff;
-int exp;
-}term;
-typedef struct{
+typedef struct
+{
+    int coeff;
+    int exp;
+} term;
+typedef struct
+{
     int n;
     term *terms;
-}poly;
-void create(poly *p){
+} poly;
+void create(poly *p)
+{
     printf("enter the number of terms :");
-    scanf("%d",&p->n);
-    p->terms =(term *)malloc(p->n*sizeof(term));
+    scanf("%d", &p->n);
+    p->terms = (term *)malloc(p->n * sizeof(term));
 
-    printf("\n Enter the terms :");
-    for(int i=0;i<p->n;i++){
-        printf("Term %d",i+1);
-        scanf("%d%d",&p->terms[i].coeff,&p->terms[i].exp);
+    printf("\n Enter the Terms :");
+    for (int i = 0; i < p->n; i++)
+    {
+        printf("%d Term : ", i + 1);
+        scanf("%d%d", &p->terms[i].coeff, &p->terms[i].exp);
     }
 }
-void display(poly p){
-    for(int i=0;i<p.n;i++){
-        printf("%dx%d+",p.terms[i].coeff,p.terms[i].exp);
+void display(poly p)
+{
+    for (int i = 0; i < p.n; i++)
+    {
+        if(i!=p.n-1){
+        printf("%dx%d+", p.terms[i].coeff, p.terms[i].exp);
+        }
+        else{
+             printf("%dx%d", p.terms[i].coeff, p.terms[i].exp);
+        }
     }
 }
-int main(){
+poly *add(poly *p1, poly *p2)
+{
+    poly *sum;
 
-    poly p;
-    create(&p);
-    display(p);
+    sum = (poly *)malloc(sizeof(poly));
+    sum->terms = (term *)malloc((p1->n + p2->n) * sizeof(term));
 
+    int i, j, k;
+    i = j = k = 0;
+    while (i < p1->n && j < p2->n)
+    {
+        if (p1->terms[i].exp > p2->terms[j].exp)
+        {
+            sum->terms[k++] = p1->terms[i++];
+        }
+        else if (p1->terms[i].exp < p2->terms[j].exp)
+        {
+            sum->terms[k++] = p2->terms[j++];
+        }
+        else
+        {
+            sum->terms[k].exp = p1->terms[i].exp;
+            sum->terms[k++].coeff = p1->terms[i++].coeff + p2->terms[j++].coeff;
+        }
+    }
+    for (; i < p1->n; i++)
+    {
+        sum->terms[k++] = p1->terms[i];
+    }
+
+    for (; j < p2->n; j++)
+    {
+        sum->terms[k++] = p2->terms[j];
+    }
+
+    sum->n = k;
+    return sum;
+}
+int main()
+{
+
+    poly p1, p2, *p3;
+    create(&p1);
+    create(&p2);
+
+    p3 = add(&p1, &p2);
+    printf("\n");
+    display(p1);
+    printf("\n");
+    display(p2);
+    printf("Addition is : \n");
+    display(*p3);
+
+    free(p1.terms);
+    free(p2.terms);
+    free(p3->terms);
+    free(p3);
     return 0;
 }
