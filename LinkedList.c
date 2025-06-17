158ec076387c26136155712c67daf98ee03e4e7e
janardan04 <janardhanborse2003@gmail.com>
2025-06-17 21:32:49 +0530

    add insert function

--- LinkedList.c
+++ LinkedList.c
@@ -1,43 +1,62 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-
-typedef struct Node {
+typedef struct Node
+{
     int data;
-    struct Node *next;
-}Node;
+    struct Node *next; // self refrential
+} Node;
 
-Node *first=NULL;
+Node *first = NULL; // head
 
-void create(int arr[],int n){
- Node *p;
- Node *last;
+void create(int arr[], int n)
+{
+    Node *p; // pointer    |data|next|
+    Node *curr;
 
- first=(Node *)malloc(sizeof(Node));
+    first = (Node *)malloc(sizeof(Node));
 
- first->data=arr[0];
- first->next=NULL;
- last=first;
+    first->data = arr[0];
+    first->next = NULL;
+    curr = first;
 
-for(int i=1;i<n;i++){
-    p=(Node *)malloc(sizeof(Node));
-    p->data=arr[i];
-    p->next=NULL;
-    last->next=p;
-    last=p;
-}
-}
-void display(Node *p){
-    while(p!=NULL){
-        printf("%d ",p->data);
-        p=p->next;
+    for (int i = 1; i < n; i++)
+    {
+        p = (Node *)malloc(sizeof(Node));
+        p->data = arr[i];
+        p->next = NULL;
+        curr->next = p;
+        curr = p;
     }
 }
 
-int main(){
+void display(Node *p)
+{
+    while (p != NULL)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+}
+void insert(Node **head,int element){
+    Node *p;
+    p=(Node *)malloc(sizeof(Node ));
+    p->data=element;
+    p->next=*head;
+    *head=p;
 
-    int a[]={1,2,3,4,5,6};
-    create(a,6);
+    
+
+}
+int main()
+{
+
+    int a[] = {1, 2, 3, 4, 5, 6};
+    create(a, 6);
     display(first);
+    insert(&first,23);
+        display(first);
+
+
     return 0;
 }
